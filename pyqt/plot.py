0cfb2bc070cc937669449b79a0b63c80c9d7fff0
imad-miftek <imad@miftek.com>
2025-03-24 20:43:04 -0400

    issue creation

--- pyqt/plot.py
+++ pyqt/plot.py
@@ -1,7 +1,9 @@
-from PySide6.QtWidgets import QMainWindow, QApplication, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QMainWindow, QApplication, QVBoxLayout, QWidget, QGraphicsScene, QGraphicsView
+from PySide6.QtCore import Qt, Signal
 import pyqtgraph as pg
 
 class Plot(QWidget):
+
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Base Plot")
@@ -17,11 +19,43 @@ class Plot(QWidget):
         layout.addWidget(self.plot)
 
         roi = pg.RectROI([0, 0], [1, 1], pen='r')
+        roi.setAcceptedMouseButtons(pg.QtCore.Qt.MouseButton.LeftButton)
+        roi.sigClicked.connect(lambda: print("Clicked"))
+        roi.sigHoverEvent.connect(lambda:  print("Hovering"))
+        roi.sigRegionChangeStarted.connect(lambda: print("Region change started"))
+        roi.sigRegionChangeFinished.connect(lambda: print("Region change ended"))
         self.plot.addItem(roi)
-        
 
+
+
+# When using the PlotWidget in a QGraphicsProxyWidget, the ROI does not respond to mouse events.
 if __name__ == '__main__':
     app = QApplication([])
-    window = Plot()
-    window.show()
+    plot = Plot()
+    view = QGraphicsView()
+    scene = QGraphicsScene()
+    proxy = scene.addWidget(plot)
+    view.setScene(scene)
+    view.show()
     app.exec()
+
+# # When using the PlotWidget as a standalone widget, the ROI responds to mouse events.
+# if __name__ == '__main__':
+#     app = QApplication([])
+#     plot = Plot()
+#     plot.show()
+#     app.exec()
+
+
+# # When using the PlotWidget in an QMdiSubWindow, the ROI responds to mouse events.
+# if __name__ == '__main__':
+#     app = QApplication([])
+#     window = QMainWindow()
+#     mdi = pg.QtWidgets.QMdiArea()
+#     window.setCentralWidget(mdi)
+#     sub = pg.QtWidgets.QMdiSubWindow()
+#     plot = Plot()
+#     sub.setWidget(plot)
+#     mdi.addSubWindow(sub)
+#     window.show()
+#     app.exec()
